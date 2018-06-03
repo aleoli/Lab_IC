1792dee427a4a04282279bfaefe89a0232a7b859
Alessandro Olivero <aleoli96@gmail.com>
2018-06-03 12:04:43 +0200

    lab 3 comunicazioni con testo

--- Comunicazioni/Lab3/rx.m
+++ Comunicazioni/Lab3/rx.m
@@ -2,18 +2,16 @@ Fc = 44100;
 Tc = 1/Fc;
 SpS = 100;
 
-in = load('audio.rm')';
-data = in(SpS:SpS:end) > 0.5;
+%in = load('audio.rm')';
+in = load('testo.rm');
 
-% binario to decimale
-index_out=bi2de(data);
-sig_out=codebook(index_out+1) * V;
+figure(1)
+plot(1:length(in), fftshift(abs(fft(in))))
 
 % demodulazione
 f0 = 10000;
-f_cos = cos(2*pi*f0*[0:Tc:(Tc*length(sig_out) - Tc)]);
-data_dem = f_cos .* sig_out;
-
+f_cos = cos(2*pi*f0*[0:Tc:(Tc*length(in) - Tc)]);
+data_dem = f_cos .* in;
 
 s_t = zeros(length(data_dem), 1);
 for a=1:SpS
@@ -22,6 +20,24 @@ end
 S_f = (fft(s_t));
 H_rx = S_f;
 X_f = fft(data_dem);
-Y_f = H_rx .* X_f;
+Y_f = H_rx' .* X_f;
 y = ifft(Y_f);
 
+
+figure(2)
+plot(1:length(Y_f), fftshift(abs(Y_f)))
+
+
+data = y(1:SpS:end) > 0.5;
+
+% binario to decimale
+n_simboli = length(data)/nbit;
+index_out = zeros(1, n_simboli);
+sig_out = zeros(1, n_simboli);
+for a=1:n_simboli
+    data(((a-1)*nbit+1):(a*nbit))
+    index_out(a) = bi2de(data(((a-1)*nbit+1):(a*nbit)));
+    sig_out(a) = codebook(index_out(a)+1);
+end
+
+char(sig_out)
--- Comunicazioni/Lab3/tx.m
+++ Comunicazioni/Lab3/tx.m
@@ -1,43 +1,64 @@
 clear all
 close all
 
-[data,Fs]=audioread('../Lab1/Audio/KDE_Startup.wav');
-data=data(:,1)';
+%[data,Fs]=audioread('../Lab1/Audio/KDE_Startup.wav');
+%data=data(:,1)';
+Fs = 44100;
 
-V = max(abs(data));
-nbit=1;
+size = 0;
+FID = fopen('testo.txt');
+while (~feof(FID))
+    riga = fgetl(FID);
+    data((size+1):(size+length(riga))) = riga;
+    size = size + length(riga) + 1;
+    %data((size-1):size) = "\n";
+end
+fclose(FID);
+%data = compose(data);
+data = double(data);
+
+
+V = 256; %max(abs(data));
+nbit = 9;
 M=2^nbit;
-DeltaV=2*V/M;
-pe=1e-5;
+DeltaV = 2*V/M;
 Tc = 1/Fs;
 SpS = 100;
 Fc = Fs;
 Rs = Fc/SpS;
 %B = 1/(2*Tc);
 
-data_ = data*(1/V);
+%data_ = data*(1/V);
 
 f0 = 10000;
-f_cos = cos(2*pi*f0*[0:Tc:(Tc*length(data_) - Tc)]);
-data_mod = f_cos .* data_;
+f_cos = cos(2*pi*f0*[0:Tc:(Tc*length(data) - Tc)]);
+data_mod = f_cos .* data;
 
 partition=[-V+DeltaV:DeltaV:V-DeltaV];
-codebook=[-V+DeltaV/2:DeltaV:V-DeltaV/2];
-[index,quants]=quantiz(data_mod,partition,codebook);
-
+codebook=[-V+DeltaV:DeltaV:V];
+[index,quants]=quantiz(data,partition,codebook);
 word=de2bi(index,nbit)';
 
-figure(1)
-plot(1:length(f_cos), fftshift(abs(fft(data_))))
-
-figure(2)
-plot(1:length(f_cos), fftshift(abs(fft(data_mod))))
-
-v_t = zeros(1, (length(word)-1)*SpS);
-for a=0:length(word)-1
-    for b=1:SpS
-        v_t(a*SpS+b) = word(a+1);
+clear size;
+w_dim = size(word);
+v_t = zeros(1, (w_dim(1))*(w_dim(2))*SpS);
+for a=0:w_dim(2)-1
+    for b=0:w_dim(1)-1
+        %v_t(a*w_dim(1)+b+1) = word(b+1, a+1);
+        for c=1:SpS
+            v_t(a*w_dim(1)*SpS+b*SpS+c) = word(b+1, a+1);
+        end
     end
 end
 
-save audio.rm v_t -ascii -double
+figure(1)
+plot(1:length(v_t), fftshift(abs(fft(v_t))))
+
+f0 = 10000;
+f_cos = cos(2*pi*f0*[0:Tc:(Tc*length(v_t) - Tc)]);
+v_t_mod = f_cos .* v_t;
+
+figure(2)
+plot(1:length(v_t_mod), fftshift(abs(fft(v_t_mod))))
+
+save testo.rm v_t_mod -ascii -double
